f664ec205020dbb7edd98aa58ccf60a7b330bb9e
Prashanth Padmanabh <prashanth128@gmail.com>
2013-01-16 19:04:08 -0500

    updated event listener

--- appengine/resources/js/script.js
+++ appengine/resources/js/script.js
@@ -10,12 +10,12 @@ $(function(){
 
     window.app = document.getElementById('app');
     window.video = document.getElementById('monitor');
-    window.red_button = document.getElementById('red-button');
     window.canvas = document.getElementById('photo');
     window.effect = document.getElementById('effect');
     window.gallery = document.getElementById('gallery');
     window.ctx = canvas.getContext('2d');
     window.intervalId = null;
+    window.localMediaStream = null;
     window.idx = 0;
     window.filters = [
       'grayscale',
@@ -28,7 +28,6 @@ $(function(){
       'invert',
       ''
     ];
-    window.initialized = false;
   } else {
     alert('Audio and video capture is not supported in your browser');
   }
@@ -49,6 +48,7 @@ function gotStream(stream) {
   } else {
     video.src = stream; // Opera.
   }
+  localMediaStream = stream; 
 
   video.onerror = function(e) {
     stream.stop();
@@ -56,20 +56,24 @@ function gotStream(stream) {
 
   stream.onended = noStream;
 
-  video.onloadedmetadata = function(e) { // Not firing in Chrome. See crbug.com/110938.
-    button.textContext = 'Take Snapshot';
-    document.getElementById('splash').hidden = true;
-    document.getElementById('app').hidden = false;
-  };
-
-  // Since video.onloadedmetadata isn't firing for getUserMedia video, we have
-  // to fake it.
-  setTimeout(function() {
+  video.addEventListener('loadedmetadata',function(e) { // Not firing in Chrome. See crbug.com/110938.
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     document.getElementById('splash').hidden = true;
     document.getElementById('app').hidden = false;
-  }, 50);
+    $('#red-button').text('Take Snapshot');
+  });
+
+  // Since video.onloadedmetadata isn't firing for getUserMedia video, we have
+  // to fake it.
+  // UPDATE: no need to fake it anymore with the event listener added
+  // setTimeout(function() {
+  //   canvas.width = video.videoWidth;
+  //   canvas.height = video.videoHeight;
+  //   document.getElementById('splash').hidden = true;
+  //   document.getElementById('app').hidden = false;
+  //   $('#red-button').text('Take Snapshot');
+  // }, 200);
 }
 
 function noStream(e) {
@@ -78,41 +82,38 @@ function noStream(e) {
     msg = 'User denied access to use camera.';
   }
   $('#errorMessage .default').hide();
-  red_button.disabled = true;
+  $('#red-button').attr('disabled', true);
   var $error_message = $('<span>'+msg+'</span>');
   $('#errorMessage').append($error_message);
   setTimeout(function(){
     $error_message.remove();
     $('#errorMessage .default').show();
-    red_button.disabled = false;
+    $('#red-button').attr('disabled', false);
   }, 5000);
 }
 
 function capture() {
-
+  if(localMediaStream){
     ctx.drawImage(video, 0, 0);
     var img = document.createElement('img');
-    img.src = canvas.toDataURL('image/png');
-
+    img.src = canvas.toDataURL('image/webp');
     var angle = Math.floor(Math.random() * 36);
     var sign = Math.floor(Math.random() * 2) ? 1 : -1;
     img.style.webkitTransform = 'rotateZ(' + (sign * angle) + 'deg)';
-
     var maxLeft = document.body.clientWidth;
     var maxTop = document.body.clientHeight;
-
     img.style.top = Math.floor(Math.random() * maxTop) + 'px';
     img.style.left = Math.floor(Math.random() * maxLeft) + 'px';
 
     gallery.appendChild(img);
-
+  }
 }
 
 
 function addTwitter(){
   setTimeout(function(){
     $('body').append('<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>');
-  }, 300)
+  }, 300);
 }
 
 function play(){
@@ -125,9 +126,7 @@ function init(el) {
     document.getElementById('errorMessage').innerHTML = 'Sorry. <code>navigator.getUserMedia()</code> is not available.';
     return;
   }
-  if(initialized){
-    el.onclick = capture;
-  }
+  el.onclick = capture;
   navigator.getUserMedia({video: true}, gotStream, noStream);
 }
 
